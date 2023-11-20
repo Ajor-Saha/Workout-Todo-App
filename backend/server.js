a09e6af11b39bada3e03c54154a295b7fa8063ca
ajor-saha <sokarama79@gmail.com>
2023-11-20 20:42:50 +0600

    Update

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const app = express()
 
 app.use(express.json())
 
+app.use("/", (req,res) => {
+    res.json({ message: "Hello from Express App"});
+})
+
 app.use((req, res, next) => {
     console.log(req.path,req.method);
     next()
