ac287d2fd9cb4175e23ddedb0eca95ede2b4084d
ajor-saha <sokarama79@gmail.com>
2023-12-31 23:44:13 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const app = express()
+const cors = require('cors');
 
 app.use(express.json())
+app.use(cors());
 
 app.use("/", (req,res) => {
     res.json({ message: "Hello from Express App"});
