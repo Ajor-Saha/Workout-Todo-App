c9ee95778fee723a2f3ce09697851a593278df56
ajor-saha <sokarama79@gmail.com>
2023-12-31 23:48:25 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const cors = require('cors');
 app.use(express.json())
 app.use(cors());
 
-app.use("/", (req,res) => {
-    res.json({ message: "Hello from Express App"});
-})
+
 
 app.use((req, res, next) => {
     console.log(req.path,req.method);
