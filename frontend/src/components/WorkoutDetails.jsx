34e1299850709a7edaa2c8fdcbb95f374016ec09
ajor-saha <sokarama79@gmail.com>
2024-01-01 00:14:36 +0600

    update

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -11,7 +11,7 @@ const WorkoutDetails = ({ workout }) => {
         if (!user) {
             return
         }
-        const response = await fetch('/api/workouts/' + workout._id, {
+        const response = await fetch('https://workout-todo-app.vercel.app/api/workouts/' + workout._id, {
             method: 'DELETE',
             headers: {
                 'Authorization': `Bearer ${user.token}`
